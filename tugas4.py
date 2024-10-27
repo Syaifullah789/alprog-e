0b0904acf989a5b38caa33c31fcdd0a8c0da5f5c
Fulla03 <pamanfulla@gmail.com>
2024-10-27 23:23:39 +0800

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,11 @@
+nama = "syaifullah"
+alamat = "dompu"
+nim = "24241148"
+nimpertama = 24
+nimakhir = 48
+hasil = nimpertama + nimakhir
+
+print("nama saya adalah"+ nama)
+print("saya berasal dari"+ alamat)
+print("nim saya"+nim)
+print(f"hasil perjumlahan dari{nimpertama} + {nimakhir} = {hasil}")
