ad7656d3c0f01f39a074660961ad7c29522e1341
Fulla03 <pamanfulla@gmail.com>
2024-11-08 20:33:32 +0800

    Add files via upload

---
+++ tugas6.py
@@ -0,0 +1,30 @@
+x = 9
+y = 6
+print("nilai dari x : ", x)
+
+
+# aturan penulisan variable
+luas = 30 
+syaifullah = 100 # jika lebih dari satu kata, gabung dengan tanda " _ "
+fulla66 = 20
+FULLA77 = 3 # boleh gabungan capital non-capital
+FULLA77 = 14
+fulla66 = 10 # jika menggabungkan huruf dan angka, angka tidak boleh berada di posisi awal
+print("Nilai dari fulla", fulla66)
+
+
+#tipe data
+a = 4 # integer bilangan bulat
+print("tipe data dari  a : ", type(a)) 
+
+b = 6.9 # float bilangan pecahan
+print("tipe data dari b :", type(b))
+
+c = "syaifullah"
+print("tipe data dari c :", type(c))
+
+d = False
+print("tipe data dari d : ", type(d))
+
+
+
