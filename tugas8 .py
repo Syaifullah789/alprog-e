6e8f137433817ce6eb304f5e0f0ec5a759a357cd
Fulla03 <pamanfulla@gmail.com>
2024-12-06 21:33:34 +0800

    Add files via upload

---
+++ tugas8 .py
@@ -0,0 +1,32 @@
+# artimatika
+
+x=24
+y=76
+
+# operasi perjumlahan
+hasil = x + y
+print(x, "+", y, "=", hasil)
+
+# operasi pengurangan
+hasil = x - y
+print(x, "-", y, "=", hasil)
+
+# operasi perkalian
+hasil = x * y
+print(x, "*", y, "=", hasil)
+
+# operasi pembagian
+hasil = x / y
+print(x, "/", y, "=", hasil)
+
+# operasi modulus
+hasil = x % y
+print(x, "%", y, "=", hasil)
+
+# operasi pangkat
+hasil = x ** y
+print(x, "**", y, "=", hasil)
+
+# operasi floor divisiaon
+hasil = x // y
+print(x, "//", y, "=", hasil)
