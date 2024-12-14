5e96fbf457f4c41f6c02a77d235d1e460b745320
Fulla03 <pamanfulla@gmail.com>
2024-12-14 15:25:38 +0800

    Add files via upload

---
+++ tugas9.py
@@ -0,0 +1,12 @@
+# latihan oprasi aritmatika
+
+nilai = float(input("masukan nilai suhu dalam celcius :"))
+
+fahrenheit = (nilai * 9/5)+32
+print("nilai dari fahrenheit =" , fahrenheit)
+
+kenzo = nilai + 273,15
+print("nilai dari kenzo =" , kenzo)
+
+reamur = (nilai * (5/4)+0)
+print("nilai dari reamur=" ,reamur)
